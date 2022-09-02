fc99a4f060b3a24df9419d4f6566331231dbfb7b
rodfval.fv@gmail.com <2Fa74lov$>
2022-09-03 01:36:27 +0200

    added echo

--- generate_subghz_playlist.sh
+++ generate_subghz_playlist.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-#GENERATES PLAYLIST FILES BY FOLDER
-
 DIR="/f/subghz"
 
-find $DIR -type f -name "*.sub" -exec sh -c 'name="$(realpath "$(dirname "{}")")/$(basename "{}")"; name="sub: /ext${name:2}"; dir="/f/subghz/playlist/$(basename "$(dirname "$(dirname "{}")")")$(basename "$(dirname "{}")").txt"; dir="${dir//-}"; echo "$name" >> "$dir";' \;
+find $DIR -type f -name "*.sub" -exec sh -c 'name="$(realpath "$(dirname "{}")")/$(basename "{}")"; name="sub: /ext${name:2}"; dir="/f/subghz/playlist/$(basename "$(dirname "$(dirname "{}")")")$(basename "$(dirname "{}")").txt"; dir="${dir//-}"; echo "$name"; echo "$name" >> "$dir";' \;
