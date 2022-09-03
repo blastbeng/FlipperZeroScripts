8ef61d673918281b10eaeee178e0d38029483c1f
rodfval.fv@gmail.com <2Fa74lov$>
2022-09-03 11:01:31 +0200

    fixes

--- generate_subghz_playlist.sh
+++ generate_subghz_playlist.sh
@@ -1,5 +1,23 @@
 #!/bin/sh
-
 DIR="/f/subghz"
 
+
+
+execute() { 
+    dirname="$(dirname "$1")"
+    name="$(realpath "$dirname")/$(basename "$1")"; 
+    name="sub: /ext${name:2}"; 
+    dir="/f/subghz/playlist/$(basename "$(dirname "$dirname")")$(basename "$dirname").txt"; 
+    dir="${dir//-}"; 
+    dir="${dir//_}"; 
+    echo "$name >> $dir"; 
+    echo "$name" >> "$dir"; 
+}
+
+export -f execute
+
+#find $DIR -type f -name "*.sub" -exec bash -c 'execute "{}"' -mtime -1h \;
 find $DIR -type f -name "*.sub" -exec sh -c 'name="$(realpath "$(dirname "{}")")/$(basename "{}")"; name="sub: /ext${name:2}"; dir="/f/subghz/playlist/$(basename "$(dirname "$(dirname "{}")")")$(basename "$(dirname "{}")").txt"; dir="${dir//-}"; dir="${dir//_}"; echo "$name"; echo "$name" >> "$dir";' \;
+
+
+#find $DIR -type f -name "*.sub"
